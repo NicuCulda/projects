56f548b6a81b2f8144ada6a9cc6855bfbdf16ba2
Nicolae Culda <nicolaeculda04@gmail.com>
2022-09-07 11:52:03 +0300

    contact form done, no stiling

--- my-resume-react-app/src/Pages/ContacMe/ContactMe.jsx
+++ my-resume-react-app/src/Pages/ContacMe/ContactMe.jsx
@@ -2,75 +2,63 @@ import './ContactMe.css'
 import React from 'react'
 import {useState} from "react"
 import contactimg from '../../Images/contact.jpg'
+import phone from '../../Images/telephone-fill.svg'
+import envelope from '../../Images/envelope-fill.svg'
+import map from '../../Images/geo-alt-fill.svg'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser';
+
 
 const ContactMe = () => {
-    const formInitialDetails = {
-        firstName: '';
-        lastName: '';
-        email: '';
-        phone: '';
-        message: '';
+    const formRef = useRef()
+    const [done, setDone] = useState(false)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        emailjs.sendForm(
+            'service_wkksfdn', 
+            'template_qbc4xf8', 
+            formRef.current, 'PiaIztszjHLVWDArf')
+      .then((result) => {
+          console.log(result.text);
+          setDone(true)
+      }, (error) => {
+          console.log(error.text);
+      });
     }
-    
-    const [formDetails, setFormDetails] = useState(formInitialDetails);
-    const[buttonText, setButtonText] = useState('Send');
-    const [status, setStatus] = useState({});
-    
-    const onFormUpdate = (category, value) => {
-        setFormDetails({
-            ...formDetails,
-            [category]: value
-        })
-    }
-    const handleSubmit = () => {
-        
-    }
+
+
 
     return (
-        <section className='contact' id='connect'>
-            <container>
-                <row className="align-items-center">
-                    <col md={6}>
-                        <img src={contactimg} alt="Contact Us" />
-                    </col>
-                    <col md={6}>
-                        <h2>Get In Touch</h2>
-                        <form onSubmit={handleSubmit}>
-                            <row>
-                                <col sm={6} className="px-1">
-                                    <input type="text" value={formDetails, firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                                </col>
-                                <col sm={6} className="px-1">
-                                    <input type="text" value={formDetails, lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
-                                </col>
-                                <col sm={6} className="px-1">
-                                    <input type="email" value={formDetails, email} placeholder="Email" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                                </col>
-                                <col sm={6} className="px-1">
-                                    <input type="tel" value={formDetails, phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
-                                </col>
-                                <col>
-                                    <textarea row="6" value={formDetails, message} placeholder="Message Me"  onChange={(e) => onFormUpdate('message', e.target.value)} />
-                                    <button type='submit'><span>{buttonText}</span></button>
-                                </col>
-                                <col>
-                                {
-                                    status.message &&
-                                    <col>
-                                        <p className={status.success === false ? "danger" : "success"}>{status.message}></p>
-                                    </col>
-                                }
-                                </col>
-                            </row>
-                        </form>
-                    </col>
-                </row>
-            </container>
-        </section>
-            
+        <div className='contact'>
+            <div className="contact-bg"></div>
+            <div className='contact-wrapper'>
+                <div className="contact-left">
+                    <h1 className='contact-title'>Contact Me</h1>
+                    <div className="contact-info">
+                        <div className="contact-info-item">
+                            <img className='contact-icon' src={phone} alt="" /> +4 0753 046 160
+                        </div>
+                        <div className="contact-info-item">
+                            <img className='contact-icon' src={envelope} alt="" /> culda_nicu@yahoo.com
+                        </div>
+                        <div className="contact-info-item">
+                            <img className='contact-icon' src={map} alt="" /> Cluj Napoca, Romania
+                        </div>
+                    </div>
+                </div>
+                <div className="contact-right">
+                    <form ref={formRef} onSubmit={handleSubmit}>
+                        <input type="text" placeholder='Name' name='user_name' />
+                        <input type="text" placeholder='Subject' name='user_subject'/>
+                        <input type="email" placeholder='Email' name='user_email' />
+                        <textarea rows="5" placeholder='Message' name='message' />
+                        <button>Submit</button>
+                        {done && "Thank you..."}
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
-
-
 
 export default ContactMe
