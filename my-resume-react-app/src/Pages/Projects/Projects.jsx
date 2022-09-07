ba1555a823b477118c307344d0721303fa96b96a
Nicolae Culda <nicolaeculda04@gmail.com>
2022-09-07 18:48:35 +0300

    added links to projects

--- my-resume-react-app/src/Pages/Projects/Projects.jsx
+++ my-resume-react-app/src/Pages/Projects/Projects.jsx
@@ -18,11 +18,13 @@ const Projects = () => {
                     <Card.Body>
                     <Card.Title>My Shopping App</Card.Title>
                     <Card.Text>
-                        This is a longer card with supporting text below as a natural
-                        lead-in to additional content. This content is a little bit
-                        longer.
+                    App Description : Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Rem iusto reprehenderit iste veniam laborum recusandae ipsum asperiores quasi!
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <a href="https://github.com/NicuCulda/Homeworks/tree/main/my-shopping-website-with-react">
+                        <Button variant="primary">Go to GitHub</Button>
+                    </a>
+                    
                     </Card.Body>
                 </Card>
                 </Col>
@@ -30,13 +32,15 @@ const Projects = () => {
                 <Card>
                     <Card.Img variant="top" src="holder.js/100px160" />
                     <Card.Body>
-                    <Card.Title>My Shopping App</Card.Title>
+                    <Card.Title>My wheather App with API</Card.Title>
                     <Card.Text>
-                        This is a longer card with supporting text below as a natural
-                        lead-in to additional content. This content is a little bit
-                        longer.
+                    App Description : Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Rem iusto reprehenderit iste veniam laborum recusandae ipsum asperiores quasi!
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <a href="https://github.com/NicuCulda/Homeworks/tree/main/Weather%20app%20-%20API">
+                        <Button variant="primary">Go to GitHub</Button>
+                    </a>
+                    
                     </Card.Body>
                 </Card>
                 </Col>
@@ -46,13 +50,15 @@ const Projects = () => {
                 <Card>
                     <Card.Img variant="top" src="holder.js/100px160" />
                     <Card.Body>
-                    <Card.Title>My Shopping App</Card.Title>
+                    <Card.Title>Responsive Design</Card.Title>
                     <Card.Text>
-                        This is a longer card with supporting text below as a natural
-                        lead-in to additional content. This content is a little bit
-                        longer.
+                        App Description : Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Rem iusto reprehenderit iste veniam laborum recusandae ipsum asperiores quasi!
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <a href="https://github.com/NicuCulda/Responsive_Design">
+                        <Button variant="primary">Go to GitHub</Button>
+                    </a>
+                    
                     </Card.Body>
                 </Card>
                 </Col>
@@ -62,11 +68,12 @@ const Projects = () => {
                         <Card.Body>
                         <Card.Title>Rock,Scisors and Paper</Card.Title>
                         <Card.Text>
-                            This is a longer card with supporting text below as a natural
-                            lead-in to additional content. This content is a little bit
-                            longer.
+                        App Description : Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Rem iusto reprehenderit iste veniam laborum recusandae ipsum asperiores quasi!
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <a href="https://github.com/NicuCulda/Homeworks/tree/main/Rock%2C%20Paper%20%26%20Scissors%20Dome%20Manipulation">
+                            <Button variant="primary">Go to GitHub</Button>
+                        </a>               
                         </Card.Body>
                     </Card>
                     </Col>
